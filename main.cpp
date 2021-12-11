13fa90fe11bf3db2c572620a434a5318af6bc5fc
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2021-12-11 17:35:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,161 +1,146 @@
 
-/*Реализуйте структуру данных из n элементов a1,a2…an, поддерживающую следующие операции:
- 
-присвоить элементу ai значение j;
-найти знакочередующуюся сумму на отрезке от l до r включительно (al−al+1+al+2−…±ar).
+/*Дано число N и последовательность из N целых чисел. 
+Найти вторую порядковую статистику на заданных диапазонах.
+
+Для решения задачи используйте структуру данных Sparse Table. 
+Требуемое время обработки каждого диапазона O(1). 
+Время подготовки структуры данных O(nlogn).
+
 Входные данные
-В первой строке входного файла содержится натуральное число n (1≤n≤105) — длина массива.
- Во второй строке записаны начальные значения элементов (неотрицательные целые числа, не превосходящие 104).
- 
-В третьей строке находится натуральное число m (1≤m≤105) — количество операций.
- В последующих m строках записаны операции:
- 
-операция первого типа задается тремя числами 0 i j (1≤i≤n, 1≤j≤104).
-операция второго типа задается тремя числами 1 l r (1≤l≤r≤n).
+В первой строке заданы 2 числа: размер последовательности N и количество диапазонов M.
+
+Следующие N целых чисел задают последовательность. 
+Далее вводятся M пар чисел - границ диапазонов.
+
 Выходные данные
-Для каждой операции второго типа выведите на отдельной строке соответствующую знакочередующуюся сумму.*/
+Для каждого из M диапазонов напечатать 
+элемент последовательности - 2ю порядковую статистику. 
+По одному числу в строке.*/
  
 #include <iostream>
 #include <vector>
  
-template <typename T, class Arithmetic>
-class [[maybe_unused]] Fenwick {
-private:
- 
-    std::vector<T> data;
-    Arithmetic foo;
- 
-public:
- 
-    [[maybe_unused]] explicit Fenwick(std::vector<T> base);
- 
-    [[maybe_unused]] void change(long long index, T value);
- 
-    [[maybe_unused]] [[maybe_unused]] void set(long long index, T value);
- 
-    [[maybe_unused]] T sum(long long index);
- 
-    [[maybe_unused]] T sum(long long left, long long right); // []
- 
-};
- 
-template<typename T, class Arithmetic>
-[[maybe_unused]] Fenwick<T, Arithmetic>::Fenwick(std::vector<T> base) {
-    data = std::vector<T> (base.size(), foo.neutral());
-    for (long long i = 0; i < base.size(); ++i) {
-        change(i, base[i]);
-    }
+long long nextPowerOfTwo(unsigned long long a) {
+    a--;
+    a |= a >> 1;
+    a |= a >> 2;
+    a |= a >> 4;
+    a |= a >> 8;
+    a |= a >> 16;
+    a |= a >> 32;
+    a++;
+    return a; // NOLINT(cppcoreguidelines-narrowing-conversions)
 }
  
-template<typename T, class Arithmetic>
-[[maybe_unused]] void Fenwick<T, Arithmetic>::change(long long index, T value) {
-    while (index < data.size()) {
-        data[index] = foo(data[index], value);
-        index |= index + 1;
+long long powerOfTwo(long long a) {
+    long long ans = 0;
+    while (a != 0) {
+        a = a >> 1;
+        ++ans;
     }
-}
- 
-template<typename T, class Arithmetic>
-[[maybe_unused]] void Fenwick<T, Arithmetic>::set(long long index, T value) {
-    change(index, foo(value, foo.opposite(sum(index, index))));
-}
- 
-template<typename T, class Arithmetic>
-[[maybe_unused]] T Fenwick<T, Arithmetic>::sum(long long index) {
-    T ans = foo.neutral();
-    while (index >= 0) {
-        ans = foo(ans, data[index]);
-        index = (index & (index + 1)) - 1;
-    }
+    --ans;
     return ans;
 }
  
-template<typename T, class Arithmetic>
-[[maybe_unused]] T Fenwick<T, Arithmetic>::sum(long long left, long long right) {
-    if (left > right) {
-        return foo.neutral();
-    }
-    if (left == 0) {
-        return sum(right);
-    }
-    return foo(sum(right), foo.opposite(sum(left - 1)));
+template <typename T, class Foo>
+class SparseTable {
+private:
+ 
+    std::vector<T> data;
+    long long width;
+    long long height;
+    /*
+     0     1       2    ...
+     w   w + 1   w + 2  ...
+     ...
+     */
+ 
+    Foo foo;
+ 
+    T& get (long long row, long long column);
+    
+public:
+    
+    explicit SparseTable(std::vector<T> base);
+ 
+    T request (long long left, long long right);
+ 
+};
+ 
+template<class T, class Foo>
+T &SparseTable<T, Foo>::get(long long int row, long long int column) {
+    return data[width * row + column];
 }
  
-template <typename T>
-class Digits {
+template<class T, class Foo>
+SparseTable<T, Foo>::SparseTable(std::vector<T> base) {
+    // assign variables
+    width = base.size();
+    height = powerOfTwo(nextPowerOfTwo(width));
+    data = std::vector<T> (height * width, foo.neutral());
+ 
+    //build the table
+    for (long long i = 0; i < width; ++i) {
+        get(0, i) = base[i];
+    }
+    for (long long i = 1; i < height; ++i) {
+        for (long long j = 0; j + (1 << i) <= width; ++j) {
+            get(i, j) = foo(get(i - 1, j), get(i - 1, j + (1 << (i - 1))));
+        }
+    }
+}
+ 
+template<typename T, class Foo>
+T SparseTable<T, Foo>::request(long long int left, long long int right) {
+    long long row = powerOfTwo(nextPowerOfTwo(right - left + 1)) - 1;
+    if (row < 0) {
+        row = 0;
+    }
+    long long columnLeft = left;
+    long long columnRight = right - (1 << row) + 1;
+    return foo(get(row, columnLeft), get(row, columnRight));
+}
+ 
+class secondMin {
 public:
-    T operator() (T a, T b) {
-        return a + b;
-    }
- 
-    T neutral () {
-        return 0;
-    }
- 
-    T opposite (T a) {
-        return -a;
+    std::pair<long long, long long> operator() (std::pair<long long, long long> a, std::pair<long long, long long> b) {
+        if (a.first > b.first) {
+            return b;
+        }
+        return a;
+    }              
+    
+    static std::pair<long long, long long> neutral() {
+        return {INT64_MAX, 0};
     }
 };
  
 int main() {
- 
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
  
-    int baseSize;
-    std::cin >> baseSize;
- 
-    std::vector<long long> baseEven ((baseSize / 2) + (baseSize % 2));
-    std::vector<long long> baseOdd (baseSize / 2);
-    for (int i = 0; i < baseSize; ++i) {
-        if (i % 2 == 0) {
-            std::cin >> baseEven[i / 2];
-        } else {
-            std::cin >> baseOdd[i / 2];
-        }
+    long long size, queries, tmp;
+    std::cin >> size >> queries;
+    std::vector<std::pair<long long, long long>> base(size);
+    for (long long i = 0; i < base.size(); ++i) {
+        std::cin >> tmp;
+        base[i] = std::pair(tmp, i + 1);
     }
- 
-    Fenwick<long long, Digits<long long>> evenTree(baseEven);
-    Fenwick<long long, Digits<long long>> oddTree(baseOdd);
- 
-    int queriesAmount;
-    std::cin >> queriesAmount;
- 
-    int query, a, b;
-    for (int i = 0; i < queriesAmount; ++i) {
-        std::cin >> query >> a >> b;
-        --a;
-        if (query == 1) {
-            --b;
-            switch (a % 2 + ((b % 2) * 2)) {
-                case 0:
-                    std::cout << evenTree.sum(a / 2, b / 2) -
-                    oddTree.sum(a / 2, b / 2 - 1) << '\n';
-                    break;
-                case 1:
-                    std::cout << oddTree.sum(a / 2, b / 2 - 1) -
-                    evenTree.sum(a / 2 + 1, b / 2) << '\n';
-                    break;
-                case 2:
-                    std::cout << evenTree.sum(a / 2, b / 2) -
-                    oddTree.sum(a / 2, b / 2) << '\n';
-                    break;
-                case 3:
-                    std::cout << oddTree.sum(a / 2, b / 2) -
-                    evenTree.sum(a / 2 + 1, b / 2) << '\n';
-                    break;
-                default:
-                    break;
-            }
+    SparseTable<std::pair<long long, long long>, secondMin> table (base);
+    long long left, right;
+    for (int i = 0; i < queries; ++i) {
+        std::cin >> left >> right;
+        tmp = table.request(left - 1, right - 1).second;
+        if (tmp == right) {
+            std::cout << table.request(left - 1, right - 2).first << '\n';
+            continue;
         }
-        if (query == 0) {
-            if (a % 2 == 0) {
-                evenTree.set(a / 2, b);
-            } else {
-                oddTree.set(a / 2, b);
-            }
+        if (tmp == left) {
+            std::cout << table.request(left, right - 1).first << '\n';
+            continue;
         }
+        std::cout << std::min(table.request(left - 1, tmp - 2).first,
+                              table.request(tmp, right - 1).first) << '\n';
     }
- 
 }
