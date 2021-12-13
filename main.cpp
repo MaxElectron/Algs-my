ef875f1f1abae62a5361d9941b8a048039ffe183
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2021-12-13 19:55:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,55 @@
-
+#include <iostream>
+#include <vector>
+ 
+void sortByLSByte(unsigned long long* beginning, int size, unsigned long long magnitude) {
+    std::vector<unsigned long long> buffer(size);
+    std::vector<int> indexByByte(256, 0);
+    //count bytes
+    for (unsigned long long* ptr = beginning; ptr < beginning + size; ++ptr) {
+        ++indexByByte[(*ptr / magnitude) % 256];
+    }
+    //recalculation
+    int sum = 0;
+    for (int i = 0; i < 256; ++i) {
+        sum += indexByByte[i];
+        indexByByte[i] = sum - indexByByte[i];
+    }
+    //do sort into buffer
+    for (unsigned long long* ptr = beginning; ptr < beginning + size; ++ptr) {
+        buffer[indexByByte[(*ptr / magnitude) % 256]] = *ptr;
+        ++indexByByte[(*ptr / magnitude) % 256];
+    }
+    //do sort within each byte
+    if (magnitude != 72057594037927936) { // 2 ^ 56 it is
+        sortByLSByte(&buffer[0], size, magnitude * 256);
+    }
+    //copy from buffer
+    int counter = 0;
+    for (unsigned long long* ptr = beginning; ptr < beginning + size; ++ptr) {
+        *ptr = buffer[counter];
+        ++counter;
+    }
+}
+ 
+int main() {
+ 
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+ 
+    int size;
+    std::cin >> size;
+    std::vector<unsigned long long> toSort(size);
+ 
+    for (int i = 0; i < size; ++i) {
+        std::cin >> toSort[i];
+    }
+ 
+    sortByLSByte(&toSort[0], size, 1);
+ 
+    for (int i = 0; i < size; ++i) {
+        std::cout << toSort[i] << ' ';
+    }
+ 
+    std::cout << '\n';
+}
