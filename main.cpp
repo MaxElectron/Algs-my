72626d59fd12b776d2a427256f6b363273208f2c
Alexander Chernyatin <71085941+MaxElectron@users.noreply.github.com>
2021-12-14 21:00:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,64 +1,72 @@
 /*Реализуйте структуру данных из n элементов a1,a2…an, поддерживающую следующие операции:
- 
+
 присвоить элементу ai значение j;
 найти знакочередующуюся сумму на отрезке от l до r включительно (al−al+1+al+2−…±ar).
 Входные данные
 В первой строке входного файла содержится натуральное число n (1≤n≤105) — длина массива.
  Во второй строке записаны начальные значения элементов (неотрицательные целые числа, не превосходящие 104).
- 
+
 В третьей строке находится натуральное число m (1≤m≤105) — количество операций.
  В последующих m строках записаны операции:
- 
+
 операция первого типа задается тремя числами 0 i j (1≤i≤n, 1≤j≤104).
 операция второго типа задается тремя числами 1 l r (1≤l≤r≤n).
 Выходные данные
 Для каждой операции второго типа выведите на отдельной строке соответствующую знакочередующуюся сумму.*/
- 
+
 #include <iostream>
 #include <vector>
- 
+
 template <typename T, class Arithmetic>
 class [[maybe_unused]] Fenwick {
 private:
- 
+
     std::vector<T> data;
     Arithmetic foo;
- 
+
 public:
- 
-    [[maybe_unused]] explicit Fenwick(std::vector<T> base);
- 
-    [[maybe_unused]] void change(long long index, T value);
- 
-    [[maybe_unused]] [[maybe_unused]] void set(long long index, T value);
- 
+
+    [[maybe_unused]] explicit Fenwick(const std::vector<T>& base);
+
+    [[maybe_unused]] void change(long long index, const T& value);
+
+    [[maybe_unused]] [[maybe_unused]] void set(long long index, const T& value);
+
     [[maybe_unused]] T sum(long long index);
- 
+
     [[maybe_unused]] T sum(long long left, long long right); // []
- 
+
 };
- 
+
 template<typename T, class Arithmetic>
-[[maybe_unused]] Fenwick<T, Arithmetic>::Fenwick(std::vector<T> base) {
+[[maybe_unused]] Fenwick<T, Arithmetic>::Fenwick(const std::vector<T>& base) {
     data = std::vector<T> (base.size(), foo.neutral());
-    for (long long i = 0; i < base.size(); ++i) {
-        change(i, base[i]);
+
+    data[0] = base[0];
+    for (int i = 1; i < base.size(); ++i) {
+        data[i] = data[i-1] + base[i];
     }
+
+    for (int i = base.size()-1; i > 0; --i) {
+        int next = (i & (i + 1)) - 1;
+        if (next >= 0) data[i] -= data[next];
+    }
+
 }
- 
+
 template<typename T, class Arithmetic>
-[[maybe_unused]] void Fenwick<T, Arithmetic>::change(long long index, T value) {
+[[maybe_unused]] void Fenwick<T, Arithmetic>::change(long long index, const T& value) {
     while (index < data.size()) {
         data[index] = foo(data[index], value);
         index |= index + 1;
     }
 }
- 
+
 template<typename T, class Arithmetic>
-[[maybe_unused]] void Fenwick<T, Arithmetic>::set(long long index, T value) {
+[[maybe_unused]] void Fenwick<T, Arithmetic>::set(long long index, const T& value) {
     change(index, foo(value, foo.opposite(sum(index, index))));
 }
- 
+
 template<typename T, class Arithmetic>
 [[maybe_unused]] T Fenwick<T, Arithmetic>::sum(long long index) {
     T ans = foo.neutral();
@@ -68,7 +76,7 @@ template<typename T, class Arithmetic>
     }
     return ans;
 }
- 
+
 template<typename T, class Arithmetic>
 [[maybe_unused]] T Fenwick<T, Arithmetic>::sum(long long left, long long right) {
     if (left > right) {
@@ -79,32 +87,32 @@ template<typename T, class Arithmetic>
     }
     return foo(sum(right), foo.opposite(sum(left - 1)));
 }
- 
+
 template <typename T>
 class Digits {
 public:
-    T operator() (T a, T b) {
+    T operator() (const T& a, const T& b) {
         return a + b;
     }
- 
+
     T neutral () {
         return 0;
     }
- 
-    T opposite (T a) {
+
+    T opposite (const T& a) {
         return -a;
     }
 };
- 
+
 int main() {
- 
+
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
- 
+
     int baseSize;
     std::cin >> baseSize;
- 
+
     std::vector<long long> baseEven ((baseSize / 2) + (baseSize % 2));
     std::vector<long long> baseOdd (baseSize / 2);
     for (int i = 0; i < baseSize; ++i) {
@@ -114,13 +122,13 @@ int main() {
             std::cin >> baseOdd[i / 2];
         }
     }
- 
+
     Fenwick<long long, Digits<long long>> evenTree(baseEven);
     Fenwick<long long, Digits<long long>> oddTree(baseOdd);
- 
+
     int queriesAmount;
     std::cin >> queriesAmount;
- 
+
     int query, a, b;
     for (int i = 0; i < queriesAmount; ++i) {
         std::cin >> query >> a >> b;
@@ -130,19 +138,19 @@ int main() {
             switch (a % 2 + ((b % 2) * 2)) {
                 case 0:
                     std::cout << evenTree.sum(a / 2, b / 2) -
-                    oddTree.sum(a / 2, b / 2 - 1) << '\n';
+                                 oddTree.sum(a / 2, b / 2 - 1) << '\n';
                     break;
                 case 1:
                     std::cout << oddTree.sum(a / 2, b / 2 - 1) -
-                    evenTree.sum(a / 2 + 1, b / 2) << '\n';
+                                 evenTree.sum(a / 2 + 1, b / 2) << '\n';
                     break;
                 case 2:
                     std::cout << evenTree.sum(a / 2, b / 2) -
-                    oddTree.sum(a / 2, b / 2) << '\n';
+                                 oddTree.sum(a / 2, b / 2) << '\n';
                     break;
                 case 3:
                     std::cout << oddTree.sum(a / 2, b / 2) -
-                    evenTree.sum(a / 2 + 1, b / 2) << '\n';
+                                 evenTree.sum(a / 2 + 1, b / 2) << '\n';
                     break;
                 default:
                     break;
@@ -156,5 +164,5 @@ int main() {
             }
         }
     }
- 
+
 }
